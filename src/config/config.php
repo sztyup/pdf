cf660450c3ca4aece2c73f73efab6e6835fdf0ff
sztyup <sztyup@gmail.com>
2018-04-17 17:42:08 +0200

    Basic config

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'defaults' => [
+        'author' => 'PDF generator',
+        'creator' => 'PDF generator',
+        'title' => 'default',
+        'subject' => 'someone',
+        'font' => 'freesans',
+        'orientation' => \Sztyup\Pdf\Document::ORIENTATION_PORTRAIT,
+        'size' => 'A4'
+    ]
+];
