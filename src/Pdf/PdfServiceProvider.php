48a643a4c18e82a03ecf0baac7395eadf670df97
sztyup <sztyup@gmail.com>
2018-04-17 17:42:45 +0200

    Basic service provider

--- src/Pdf/PdfServiceProvider.php
+++ src/Pdf/PdfServiceProvider.php
@@ -8,11 +8,11 @@ class PdfServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
     }
 
     public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__.'/../config/dompdf.php', 'dompdf');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'pdf');
     }
 }
